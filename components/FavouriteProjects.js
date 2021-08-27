1c2314801acc97ea0b0823e1f20210a013016a3e
davidPardoED <pardodavid10@gmail.com>
2021-08-26 22:43:22 -0500

    First Commit

---
+++ components/FavouriteProjects.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+export default function FavouriteProjects() {
+  const [showTechnologies1, setShowTechnologies1] = useState(false);
+  return (
+    <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900">
+      <div className="max-w-6xl mx-auto">
+        <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
+          <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center">
+            Favourite Projects
+          </h1>
+          <Link href="/projects">
+            <a className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-up-right-square"
+                stroke="4"
+                strokeWidth="4"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
+                />
+              </svg>
+              <p>View all</p>
+            </a>
+          </Link>
+        </header>
+
+        {/* Grid starts here */}
+        <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
+          {/* Single card */}
+          <a
+            className="w-full block col-span-3 shadow-2xl"
+          >
+            <div className="relative overflow-hidden">
+              <img
+                src="/Ami.svg"
+                alt="portfolio"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
+              />
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                AgroTICS
+              </h1>
+              <div className="z-10  absolute left-10  top-20 flex flex-col items-start justify-center ">
+                <h1 className="bg-gray-500 rounded-md px-2 my-1 py-1 bg-opacity-80 text-xl">
+                  Frontend: Angular 8, Bootstrap
+                </h1>
+                <h1 className="bg-gray-500 rounded-md px-2 my-1 py-1 bg-opacity-80 text-xl">
+                  Mobile: React Native
+                </h1>
+                <h1 className="bg-gray-500 rounded-md px-2 my-1  py-1 bg-opacity-80 text-xl">
+                  Backend: Firebase
+                </h1>
+              </div>
+              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                01
+              </h1>
+            </div>
+          </a>
+          {/* Single card */}
+          <a
+            className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
+          >
+            <div className="relative overflow-hidden">
+              {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
+              <img
+                src="/Agrotics.svg"
+                alt="portfolio"
+                className="transform hover:scale-125 transition duration-2000 ease-out"
+              />
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                AgroTICS
+              </h1>
+              <div className="z-10  absolute left-10  top-20 flex flex-col items-start justify-center ">
+                <h1 className="bg-gray-500 rounded-md px-2 my-1 py-1 bg-opacity-80">
+                  Frontend: ReactJS, Typescript
+                </h1>
+                <h1 className="bg-gray-500 rounded-md px-2 my-1 py-1 bg-opacity-80">
+                  Mobile: React Native
+                </h1>
+                <h1 className="bg-gray-500 rounded-md px-2 my-1  py-1 bg-opacity-80">
+                  Backend: NodeJs, MongoDB
+                </h1>
+              </div>
+              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                02
+              </h1>
+            </div>
+          </a>
+          {/* Single card */}
+          <a
+            className="w-full block col-span-3 sm:col-span-1  object-cover"
+          >
+            <div className="relative overflow-hidden shadow-2xl">
+              {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
+              <img
+                src="/AppLiid.png"
+                alt="portfolio"
+                className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
+              />
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
+                AppLiid
+              </h1>
+              <div className="z-10  absolute left-10  top-20 flex flex-col items-start justify-center ">
+                <h1 className="bg-gray-500 rounded-md px-2 my-1 py-1 bg-opacity-80">
+                  Mobile: React Native
+                </h1>
+                <h1 className="bg-gray-500 rounded-md px-2 my-1  py-1 bg-opacity-80">
+                  Backend: Firebase
+                </h1>
+              </div>
+              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+                03
+              </h1>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ constants/data.js
@@ -0,0 +1,100 @@
+const userData = {
+  githubUsername: "davidPardoC",
+  name: "David Pardo",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "pardodavid10@gmail.com",
+  phone: "+593 995710255",
+  address: "Loja, Ecuador",
+  projects: [
+    {
+      title: "AgroTICS",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/Agrotics.svg",
+    },
+    {
+      title: "LiiDApp",
+      link: "https://placeholdertech.in",
+      imgUrl: "/AppLiid.png",
+    },
+    {
+      title: "Artem369",
+      link: "https://manuarora.in",
+      imgUrl: "/Artem.png",
+    },
+
+    {
+      title: "Ami",
+      link: "https://algochurn.com",
+      imgUrl: "/Ami.svg",
+    },
+    {
+      title: "Trading Platform",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/Trading.png",
+    },
+    {
+      title: "CRM",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/Crm.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and always keep learning",
+    description: [
+      `I have been developing applications professionally for almost 3 years.`,
+      `I started in web development learning HTML and CSS, and I was always afraid to implement Javascript on my sites, until I took the risk of entering as a web development intern at my university, that's when it all started.`,
+      `Now I am proud to have learned and used various technologies for the development of my applications, my main stack is heavily based on Javascript / Typescript for example ReactJs,
+      Reat Native, NodeJS, Express, MongoDb, Angular ,  NestJs and some AWS with EC2, S3 and CodePipeline`,
+    ],
+    currentProject: "Handy (my secret project)",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Lead Frontend Developer",
+      company: "Exmerdev",
+      year: "2021",
+      companyLink: "https://exmerdev.com/",
+      desc: "Angular , Websockets, Highcharts, Ngx-bootstrap, Material-ui, Jira.",
+    },
+    {
+      title: "Lead React Native Intern",
+      company: "UTPL",
+      year: "2020",
+      companyLink: "https://utpl.edu.ec/",
+      desc: "Technologies: React Native, React Native Elements, Firebase, Axios, Redux, Firebase Cloud Functions.",
+    },
+    {
+      title: "Content Virtualizer",
+      company: "EDILOJA",
+      year: "2019",
+      companyLink: "http://www.ediloja.com.ec/",
+      desc: "Technologies: HTML, CSS3, Javascript.",
+    },
+    {
+      title: "Freelance",
+      company: "Self-Employed",
+      year: "2019",
+      companyLink: "https://davidpardoc.github.io/",
+      desc: "Technologies; ReactJs, React Native, NodeJs, MongoDB, PostgreSQL, Firebase",
+    },
+    {
+      title: "Intern web developer",
+      company: "UTPL",
+      year: "2019",
+      companyLink: "https://utpl.edu.ec/",
+      desc: "Technologies: Angular, Firebase, Bootstrap.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/davidcodes1/",
+    linkedin: "https://www.linkedin.com/in/david-pardo-996999153/",
+    github: "https://github.com/davidPardoC",
+  },
+};
+
+export default userData;

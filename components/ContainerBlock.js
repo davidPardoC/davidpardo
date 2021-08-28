39876b0429bf52773dd7718e612e1f62b8e860e0
davidPardoED <pardodavid10@gmail.com>
2021-08-28 09:58:38 -0500

    Change meta image

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -10,7 +10,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const meta = {
     title: "David Pardo - Developer.",
     description: `I've been developing websites for 2 years straight. Get in touch with me to know more.`,
-    image: "/avatar.png",
+    image: "/Ami.svg",
     type: "website",
     ...customMeta,
   };
